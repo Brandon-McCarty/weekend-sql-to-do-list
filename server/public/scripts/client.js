7d65a9ee214456bb4301057696284adce6e48f91
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-18 16:24:18 -0500

    add post route for server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,30 +4,30 @@ function handleReady() {
     console.log('JQ Loaded');
     // Show tasks on page load
     getToDoList();
-    
+
 }; // end handleReady
 
 // GET /toDo
 function getToDoList() {
     $.ajax({
-      type: 'GET',
-      url: '/todo'
+        type: 'GET',
+        url: '/todo'
     }).then(function (response) {
-      console.log(response);
-      renderToDoList(response);
+        console.log(response);
+        renderToDoList(response);
     }).catch(function (error) {
-      console.log('error in GET', error);
+        console.log('error in GET', error);
     });
-  }; // end getToDoList
+}; // end getToDoList
 
-  // Render all tasks on the DOM
-  function renderToDoList(toDoList) {
+// Render all tasks on the DOM
+function renderToDoList(toDoList) {
     // empty before appending to avoid repetition  
     $('#toDoList').empty();
     // loop through the object sent by database
     for (let i = 0; i < toDoList.length; i += 1) {
-      let task = toDoList[i];
-      let row = $(`
+        let task = toDoList[i];
+        let row = $(`
         <tr>
           <td>${task.task}</td>
           <td>${task.status}</td>
@@ -35,9 +35,9 @@ function getToDoList() {
           <td><button class="deleteBtn">DELETE</button></td>     
         </tr>
       `)
-  
-      row.data('task', task);
-      // For each task, append a new row to our table
-      $('#toDoList').append(row);
+
+        row.data('task', task);
+        // For each task, append a new row to our table
+        $('#toDoList').append(row);
     }
-  }; // End renderToDoList
+}; // End renderToDoList
