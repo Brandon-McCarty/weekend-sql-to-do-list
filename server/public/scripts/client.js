bf53371f55b672a4c6006fb47e44128f0150abd1
Brandon McCarty <brandon.m.mccarty12@gmail.com>
2022-03-18 16:12:04 -0500

    add render to-do list on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,39 @@ $(handleReady)
 
 function handleReady() {
     console.log('JQ Loaded');
+    getToDoList();
     
 }
+
+// GET /toDo
+function getToDoList() {
+    $.ajax({
+      type: 'GET',
+      url: '/todo'
+    }).then(function (response) {
+      console.log(response);
+      renderToDoList(response);
+    }).catch(function (error) {
+      console.log('error in GET', error);
+    });
+  }
+
+  function renderToDoList(toDoList) {
+    $('#toDoList').empty();
+
+    for (let i = 0; i < toDoList.length; i += 1) {
+      let task = toDoList[i];
+      let row = $(`
+        <tr>
+          <td>${task.task}</td>
+          <td>${task.status}</td>
+          <td><button class="markCompleteBtn">Mark As Complete</button></td>
+          <td><button class="deleteBtn">DELETE</button></td>     
+        </tr>
+      `)
+  
+      row.data('task', task);
+      // For each book, append a new row to our table
+      $('#toDoList').append(row);
+    }
+  }
